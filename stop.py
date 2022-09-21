b7b092e5a42add9e153a94fc75bfe9f6bb606242
Carlos  chen <ccjjww1222@gmail.com>
2022-09-21 10:44:40 +0800

    Create stop.py

---
+++ stop.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+import uuid
+from utils.serial_control import serial_control
+ser = serial_control()
+
+def main(cmd):
+    cmd_dict = {
+        "uuid": str(uuid.uuid1()),
+        "cmd": cmd,
+        "from": "camera",
+    }
+    ser.send_cmd(cmd_dict)
+    print(ser.get_ret())
+
+
+
+if __name__ == "__main__":
+    try:
+        main("STOP 0.")
+    except KeyboardInterrupt:
+        print("ctrl+c stop")
+        ser.close()
+
